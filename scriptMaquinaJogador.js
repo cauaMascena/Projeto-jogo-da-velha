427c19b67a3bc9ade1c762f53b08042f845aec25
brunalozinski <lozinskibruna@gmail.com>
2021-11-09 08:04:05 -0300

    first commit

--- scriptMaquinaJogador.js
+++ scriptMaquinaJogador.js
@@ -1,43 +1,89 @@
-    var vez = 1;
-    var vencedor = "";
+const player1 = 'cereja';
+const player2 = 'estrela';
+var playTime = player1;
+var gameOver = false;
 
-    function casasIguais(a, b, c){
-        var casaA = $("#casa"+a);
-        var casaB = $("#casa"+b);
-        var casaC = $("#casa"+c);
-        var bgA = $("#casa"+a).css("background-image");
-        var bgB = $("#casa"+b).css("background-image");
-        var bgC = $("#casa"+c).css("background-image");
-        if( (bgA == bgB) && (bgB == bgC) && (bgA != "none" && bgA != "")){
-            if(bgA.indexOf("1.png") >= 0)
-                vencedor = "1";
-            else
-                vencedor = "2";
-            return true;
-        }
-        else{
-            return false;
-        }
+function atualizaMostrador() {
+    if (gameOver) {
+        return;
     }
+    if (playTime == player1) {
+        var player = document.querySelectorAll('div#mostrador img')[0];
+        player.setAttribute('src', 'img/cereja.png');
+    } else {
+        var player = document.querySelectorAll('div#mostrador img')[0];
+        player.setAttribute('src', 'img/estrela.png');
+    }
+}
 
-    function verificarFimDeJogo(){
-        if( casasIguais(1, 2, 3) || casasIguais(4, 5, 6) || casasIguais(7, 8, 9) ||
-            casasIguais(1, 4, 7) || casasIguais(2, 5, 8) || casasIguais(3, 6, 9) ||
-            casasIguais(1, 5, 9) || casasIguais(3, 5, 7)
-            ){
-            $("#resultado").html("<h1>O jogador " + vencedor + "venceu! </h1>");
-            $(".casa").off("click");
-        }
+min = 1;
+max = 9;
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+async function verificarVencedor() {
+    var a1 = document.getElementById('a1').getAttribute('jogada');
+    var a2 = document.getElementById('a2').getAttribute('jogada');
+    var a3 = document.getElementById('a3').getAttribute('jogada');
+
+    var b1 = document.getElementById('b1').getAttribute('jogada');
+    var b2 = document.getElementById('b2').getAttribute('jogada');
+    var b3 = document.getElementById('b3').getAttribute('jogada');
+
+    var c1 = document.getElementById('c1').getAttribute('jogada');
+    var c2 = document.getElementById('c2').getAttribute('jogada');
+    var c3 = document.getElementById('c3').getAttribute('jogada');
+
+    var vencedor = '';
+
+    if (((a1 == b1 && a1 == c1) || (a1 == a2 && a1 == a3) || (a1 == b2 && a1 == c3)) && a1 != '') {
+        vencedor = a1;
+    } else if (
+        (b2 == b1 && b2 == b3 && b2 != '') ||
+        (b2 == a2 && b2 == c2 && b2 != '') ||
+        (b2 == a3 && b2 == c1 && b2 != '')
+    ) {
+        vencedor = b2;
+    } else if ((c3 == c2 && c3 == c1) || (c3 == a3 && c3 == b3 && c3 != '')) {
+        vencedor = c3;
     }
-    
-    $(".casa").click(function(){
-        var bg = $(this).css("background-image");
-        if(bg == "none" || bg == "")
-        {          
-            var fig = "url(" + vez.toString() + ".png)";
-            $(this).css("background", fig);
-            vez = (vez == 1? 2:1); 
-            verificarFimDeJogo();
-        }
-    });
-    
+    if (vencedor != '') {
+        gameOver = true;
+
+        await sleep(50);
+
+        alert("O ganhador foi a: '" + vencedor + "'");
+    }
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function inicializarEspacos() {
+    var espacos = document.getElementsByClassName('espaco');
+    for (var i = 0; i < espacos.length; i++) {
+        espacos[i].addEventListener('click', function () {
+            if (gameOver) {
+                return;
+            }
+            if (this.getElementsByTagName('img').length == 0) {
+                if (playTime == player1) {
+                    this.innerHTML = "<img src='img/cereja.png'>";
+                    this.setAttribute('jogada', player1);
+                    playTime = player2;
+                } else {
+                    this.innerHTML = "<img src='img/estrela.png'>";
+                    this.setAttribute('jogada', player2);
+                    playTime = player1;
+                }
+                atualizaMostrador();
+                verificarVencedor();
+            }
+        });
+    }
+}
+
+atualizaMostrador();
+inicializarEspacos();
